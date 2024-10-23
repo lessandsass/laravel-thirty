3da0b9ade26a5cf89608b480c06d75ff7f0d8a3d
Less <31947204+lessandsass@users.noreply.github.com>
2024-10-23 17:03:27 +0530

    Day 15 Understanding Database Seeders
    - The databases\seeders\DatabaseSeeder.php file was updated with first and last names
    - Some job[job listing] was added
    - How to create a specific seeder
    - The job seeder was created using the artisan command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
+
     }
 }
---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Job;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class JobSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Job::factory(100)->create();
+    }
+}
